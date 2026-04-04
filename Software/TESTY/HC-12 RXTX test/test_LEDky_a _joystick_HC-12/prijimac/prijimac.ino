07aa11bf38b4d69c14dfe1ac86c35731b2ad4574
Svitek0 <svitekmartin23@spseplzen.cz>
2026-04-04 20:27:39 +0200

    +dokumentace, 3d modely, výkresy

--- Software/TESTY/HC-12 RXTX test/test_LEDky_a _joystick_HC-12/prijimac/prijimac.ino
+++ Software/TESTY/HC-12 RXTX test/test_LEDky_a _joystick_HC-12/prijimac/prijimac.ino
@@ -1,21 +1,10 @@
-//RX
-
-#define LED_PIN 7
-
-HardwareSerial HC12(2);
-
 void setup() {
-  Serial.begin(115200);
-
-  HC12.begin(9600, SERIAL_8N1, 5, 6); 
-
-  pinMode(LED_PIN, OUTPUT);
+  USBSerial.begin(115200);
+  delay(2000);
+  USBSerial.println("Test USBSerial");
 }
 
 void loop() {
-  if (HC12.available()) {
-    uint8_t val = HC12.read(); 
-    analogWrite(LED_PIN, val);   
-    Serial.print("recv ="); Serial.println(val);
-  }
+  USBSerial.println("Loop");
+  delay(1000);
 }
